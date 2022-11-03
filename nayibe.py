1174e52092126e86f6253ef6392c03aa47b8ea2c
Justo <ju2484s@gmail.com>
2022-11-03 10:17:46 -0500

    imagen fondo

--- nayibe.py
+++ nayibe.py
@@ -7,8 +7,20 @@ screen=pg.display.set_mode(size)
 clock=pg.time.Clock()
 #variables player
 nave_player=pg.image.load("./imagenes/nave_player.png").convert_alpha()
-rectangulo_player=nave_player.get_rect(midbottom=(80,300))
+rectangulo_player=nave_player.get_rect(midbottom=(80,600))
 speed_playerx=0
+#movimiento player
+def move_player():
+    global rectangulo_player
+    global speed_playerx
+    print (rectangulo_player.right)
+    if rectangulo_player.left>690:
+        speed_playerx=0
+        rectangulo_player.left=690
+    if rectangulo_player.right<120:
+        speed_playerx=0
+        rectangulo_player.right=120
+    rectangulo_player.x+=speed_playerx
 #funcion de eventos
 def events():
     global speed_playerx
@@ -19,9 +31,9 @@ def events():
         #eventos de teclado
         if event.type==pg.KEYDOWN:
             if event.key==pg.K_LEFT:
-                speed_playerx=3
-            if event.key==pg.K_RIGHT:
                 speed_playerx=-3
+            if event.key==pg.K_RIGHT:
+                speed_playerx=3
         if event.type==pg.KEYUP:
             if event.key==pg.K_LEFT:
                 speed_playerx=0
@@ -37,6 +49,7 @@ def drawling():
 while True:
     events()
     screen.fill("White")
+    move_player()
     #zona de dibujo
     drawling()
     pg.display.update()
