b5f669a5a62b3ef660c088c4c11a9104ea8cca7b
Aikido 24 <ju2484s@gmail.com>
2022-11-10 19:27:26 -0500

    10-11-2022

--- nayibe.py
+++ nayibe.py
@@ -7,15 +7,30 @@ screen=pg.display.set_mode(size)
 clock=pg.time.Clock()
 #musica
 cancion1=pg.mixer.Sound("./musica/Out-Of-My-System.mp3")
-
+#variables enemigos
+imagen_enemigo1=pg.image.load("./imagenes/marciano 2.png").convert_alpha()
+enemigo1_posicion=imagen_enemigo1.get_rect(midbottom=(80,0))
+direcion_enemigo=6
+decenso_enemigo=0
+nave_destrucion=True
 #fondo
-fondo=pg.image.load("./imagenes/fondo.png").convert()
+fondo=pg.image.load("./imagenes/fondo space.png").convert()
 posicion_fondo=(0,0)
+#fondo particulas
+fondo_particulas=pg.image.load("./imagenes/particulas.png").convert_alpha()
+posicion_particulas=fondo_particulas.get_rect(midtop=(410,0))
+posicion_particulas2=fondo_particulas.get_rect(midbottom=(410,0))
+
 #variables player
-nave_player=pg.image.load("./imagenes/nave_player.png").convert_alpha()
+nave_player=pg.image.load("./imagenes/NAVE 2.png").convert_alpha()
 rectangulo_player=nave_player.get_rect(midbottom=(80,600))
 speed_playerx=0
 speed_playery=0
+speed_nave=7
+#disparos player
+disparos=[]
+imagen_bala=pg.image.load("./imagenes/BALAS.png").convert_alpha()
+posicion_bala=imagen_bala.get_rect(midbottom=(100,100))
 #movimiento player
 def move_player():
     global rectangulo_player
@@ -40,6 +55,9 @@ def move_player():
 def events():
     global speed_playerx
     global speed_playery
+    global imagen_bala
+    global disparos
+    global rectangulo_player
     for event in pg.event.get():
         if event.type==pg.QUIT:
             pg.quit()
@@ -47,13 +65,15 @@ def events():
         #eventos de teclado
         if event.type==pg.KEYDOWN:
             if event.key==pg.K_LEFT:
-                speed_playerx=-3
+                speed_playerx=-speed_nave
             if event.key==pg.K_RIGHT:
-                speed_playerx=3
+                speed_playerx=speed_nave
             if event.key==pg.K_UP:
-                speed_playery=-3
+                speed_playery=-speed_nave
             if event.key==pg.K_DOWN:
-                speed_playery=3
+                speed_playery=speed_nave
+            if event.key==pg.K_SPACE:
+                disparos.append(imagen_bala.get_rect(midbottom=rectangulo_player.center))
         if event.type==pg.KEYUP:
             if event.key==pg.K_LEFT:
                 speed_playerx=0
@@ -63,15 +83,65 @@ def events():
                 speed_playery=0
             if event.key==pg.K_DOWN:
                 speed_playery=0
-            
+#movimiento fondo
+def movimiento_fondo():
+    global posicion_particulas
+    global posicion_particulas2
+    posicion_particulas.bottom+=5
+    posicion_particulas2.bottom+=5
+    if posicion_particulas.top>=600:
+        posicion_particulas.bottom=0
+    if posicion_particulas2.top>=600:
+        posicion_particulas2.bottom=0
+#movimiento enemigo
+def movimiento_enemigo1():
+    global enemigo1_posicion
+    global imagen_enemigo1
+    global direcion_enemigo
+    global decenso_enemigo
+    if enemigo1_posicion.left>750:
+        direcion_enemigo=-6
+        enemigo1_posicion.top+=20
+    if enemigo1_posicion.right<50:
+        direcion_enemigo=6
+        enemigo1_posicion.top+=20
+    #decenso_enemigo+=0.007
+    #enemigo1_posicion.top+= int(decenso_enemigo)
+    enemigo1_posicion.left+=direcion_enemigo
+    #print(enemigo1_posicion.top)
 #funcion de dibujo
+def disparos_draw():
+    global disparos
+    global imagen_bala
+    global enemigo1_posicion
+    global nave_destrucion
+    if len(disparos)!=0:
+        for i in range(len(disparos)):
+            disparos[i].top-=5
+            if enemigo1_posicion.colliderect(disparos[i]):
+                nave_destrucion=False
+            screen.blit(imagen_bala,disparos[i])
+            
+        for i in range(len(disparos)):        
+            if disparos[i].bottom<-20:
+                disparos.pop(i)
+                break
 def drawling():
     global nave_player
     global rectangulo_player
     global fondo
     global posicion_fondo
+    global fondo_particulas
+    global posicion_particulas
+    global posicion_particulas2
+    global posicion_bala
+    global imagen_bala
     screen.blit(fondo,posicion_fondo)
+    screen.blit(fondo_particulas,posicion_particulas)
+    screen.blit(fondo_particulas,posicion_particulas2)
     screen.blit(nave_player,rectangulo_player)
+    disparos_draw()
+
 cancion1.play()
 #bucle principal
 while True:
@@ -79,6 +149,10 @@ while True:
     screen.fill("White")
     move_player()
     #zona de dibujo
+    movimiento_fondo()
     drawling()
+    if nave_destrucion:
+        screen.blit(imagen_enemigo1,enemigo1_posicion)
+    movimiento_enemigo1()
     pg.display.update()
     clock.tick(60)
